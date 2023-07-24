cc6be9a7f22f4bde87cf7864318282e94be49b82
garvit-density <garvit.intern@density.exchange>
2023-07-25 02:31:01 +0530

    add tags for SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="atlan garvit virmani sql editor" />
+
+        <meta name="description" content="SQL fast editor web app" />
+        <meta
+          property="og:image"
+          content="https://website-assets.atlan.com/img/atlan-blue.svg"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

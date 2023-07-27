5e6c4e71a1fe570b3fed7f3a42f82ff6f7d9094f
garvit-density <garvit.intern@density.exchange>
2023-07-27 14:43:57 +0530

    fix

--- src/Components/UI/SnackBar.jsx
+++ src/Components/UI/SnackBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SnackBar({ msg, isError = false }) {
   return (
     <div
-      className={`border px-4 py-2 fixed top-20 right-8 flex justify-between items-center rounded-full ${
+      className={`border z-50 px-4 py-2 fixed top-20 right-8 flex justify-between items-center rounded-full ${
         isError
           ? "bg-red-100 border-red-400 text-red-700"
           : "bg-green-100 border-green-400 text-green-700"

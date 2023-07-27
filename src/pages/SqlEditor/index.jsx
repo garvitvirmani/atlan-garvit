89b54a47533f8fafc8b816bec008f563d6458eed
garvit-density <garvit.intern@density.exchange>
2023-07-27 15:33:45 +0530

    fix

--- src/pages/SqlEditor/index.jsx
+++ src/pages/SqlEditor/index.jsx
@@ -10,7 +10,7 @@ function SqlEditor() {
   return (
     <div className="pt-[60px]">
       <div className="flex flex-col md:flex-row">
-        <div className="fixed top-20 left-0 w-3/12 hidden md:block">
+        <div className="fixed top-20 left-0 w-3/12 hidden md:block z-40">
           <SideBar />
         </div>
         <div className="flex flex-col items-center p-10 gap-[30px] w-full md:w-9/12 ml-auto">
